20ae9b3ad703777d845baa634d02717d263ffb2b
NF RIDOY <nfridoy@gmail.com>
2024-09-27 03:36:17 +0600

    ✨: [Start] : AntD+ TailwindCSS + DaisiUI : MainLayout

--- src/App.tsx
+++ src/App.tsx
@@ -25,10 +25,10 @@ import MainLayout from "./components/layout/MainLayout";
 // }
 
 function App() {
-
     return (
         <>
             <MainLayout />
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
         </>
     );
 }
--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Breadcrumb, Layout, Menu, theme } from "antd";
 import SidebarDrawer from "./SidebarDrawer";
 import { NavItems } from "../../utils/NavItems/NavItems";
+import Navbar from "../ui/Navbar";
+import { Outlet } from "react-router-dom";
 
 const { Header, Content, Footer } = Layout;
 
@@ -17,23 +19,18 @@ const App: React.FC = () => {
 
     return (
         <Layout>
-            <Header style={{ display: "flex", alignItems: "center" }}>
+            <Navbar />
+            {/* <Header style={{ display: "flex", alignItems: "center" }}>
                 <div className="demo-logo" />
-                <Menu
+                 <Menu
                     theme="dark"
                     mode="horizontal"
                     defaultSelectedKeys={["1"]}
                     items={items}
                     style={{ flex: 1, minWidth: 0 }}
                 />
-            </Header>
-            <SidebarDrawer />
+            </Header> */}    
             <Content style={{ padding: "0 48px" }}>
-                <Breadcrumb style={{ margin: "16px 0" }}>
-                    <Breadcrumb.Item>Home</Breadcrumb.Item>
-                    <Breadcrumb.Item>List</Breadcrumb.Item>
-                    <Breadcrumb.Item>App</Breadcrumb.Item>
-                </Breadcrumb>
                 <div
                     style={{
                         background: colorBgContainer,
@@ -41,7 +38,7 @@ const App: React.FC = () => {
                         padding: 24,
                         borderRadius: borderRadiusLG,
                     }}>
-                    Content
+                    <Outlet />
                 </div>
             </Content>
             <Footer style={{ textAlign: "center" }}>
