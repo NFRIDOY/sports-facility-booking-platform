b7fec5a21c443a1568fda63d44f4ef9ed6e54f02
NF RIDOY <nfridoy@gmail.com>
2024-10-08 03:32:29 +0600

    ✅ [Update] : login functional

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,13 @@
 import { useForm, SubmitHandler } from "react-hook-form";
 import { logo } from "../utils/images/logo";
 import { Link } from "react-router-dom";
-import toast from "react-hot-toast";
+import { usePostUserLoginMutation } from "../redux/api/api";
+import { ErrorAlert } from "../components/ui/Alerts/ErrorAlert";
+import { SuccessAlert } from "../components/ui/Alerts/SuccessAlert";
+import ErrorCommon from "../components/ui/Error/ErrorCommon";
+import Loading from "../components/ui/Loadings/Loading";
 
-type Inputs = {
+export type TLoginUser = {
     email: string;
     password: string;
 };
@@ -11,31 +15,58 @@ const Login = () => {
     const {
         register,
         handleSubmit,
-        watch,
         formState: { errors },
-    } = useForm<Inputs>();
-    const onSubmit: SubmitHandler<Inputs> = (data) => {
+    } = useForm<TLoginUser>();
+    const [
+        postUserLogin,
+        { data: postUserLoginData, isLoading: isLoadingUserLogin, isError },
+    ] = usePostUserLoginMutation();
+    const onSubmit: SubmitHandler<TLoginUser> = async (data: TLoginUser) => {
         console.log(data);
 
         // send data to server use RTK
+        await postUserLogin(data);
+        // console.log(result);
+        // await console.log("postUserLoginData : ", postUserLoginData);
+        // console.log("postUserLoginData : ", postUserLoginData);
+        // await console.log("isLoading : ", isLoadingUserLogin);
 
         // alert
-
-        toast.success("Successfully toasted!");
+        if (isError) {
+            console.log("Error: " + data);
+            ErrorAlert();
+            return;
+        }
+        if (postUserLoginData) {
+            // console.log("Success: ", postUserLoginData?.success);
+            console.log("postUserLoginData : ", postUserLoginData);
+            SuccessAlert();
+        }
     };
 
-    console.log(watch("email")); // watch input value by passing the name of it
+    // console.log(watch("email")); // watch input value by passing the name of it
+
+    if (isLoadingUserLogin) {
+        return <Loading />;
+    }
+
+    if (isError) {
+        // ErrorAlert();
+        return <ErrorCommon />;
+    }
 
     return (
         <>
             <div className="bg-sky-100 flex justify-center items-center h-screen">
                 <div className="w-1/2 h-screen hidden lg:flex justify-center items-center ">
-                    <img
-                        // src="https://img.freepik.com/fotos-premium/imagen-fondo_910766-187.jpg?w=826"
-                        src={logo}
-                        alt="Placeholder Image"
-                        className="object-cover size-96"
-                    />
+                    <Link to={"/"}>
+                        <img
+                            // src="https://img.freepik.com/fotos-premium/imagen-fondo_910766-187.jpg?w=826"
+                            src={logo}
+                            alt="Placeholder Image"
+                            className="object-cover size-96"
+                        />
+                    </Link>
                 </div>
 
                 <div className="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
