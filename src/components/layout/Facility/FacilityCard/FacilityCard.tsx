4471500f27d8f872a7d74581b7f183d332b45e2a
NF RIDOY <nfridoy@gmail.com>
2024-10-08 08:08:08 +0600

    ✅ [Update] : Facility card UI

--- src/components/layout/Facility/FacilityCard/FacilityCard.tsx
+++ src/components/layout/Facility/FacilityCard/FacilityCard.tsx
@@ -3,18 +3,15 @@ import { IFacility } from "./../../../../types/facility.interface";
 const FacilityCard = ({ facitity }: { facitity: IFacility }) => {
     return (
         <div>
-            <div className="mx-auto my-10 card w-96 h-96 bg-base-100 shadow-xl m-4 border-t-0 border-t-sky-500 hover:border-t-8">
-                <div className="card-body">
-                    <Link to={`/facility/${facitity?._id}`}>
+            <div className="mx-auto my-10 card w-96 h-80 bg-base-100 shadow-xl m-4 border-t-2 border-t-sky-500 hover:border-t-8">
+                <div className="card-body ">
+                    <Link to={`/facility/${facitity?._id}`} className="flex-grow">
                         <h2 className="card-title text-xl font-bold">
                             {facitity.name}
                         </h2>
-                        <p className="text-gray-600 mb-2">
-                            {facitity.description}
-                        </p>
                         <div className="text-sm text-gray-500 mb-4">
-                            <p className="text-2xl">
-                                Price per hour: ${facitity.pricePerHour}
+                            <p className="text-2xl primary-text font-bold">
+                                Price Per Hour: ${facitity.pricePerHour}
                             </p>
                         </div>
                         <p className="my-2">Location: {facitity.location}</p>
--- src/components/layout/Facility/FacititySection.tsx
+++ src/components/layout/Facility/FacititySection.tsx
@@ -10,10 +10,11 @@ import FacilityCard from "./FacilityCard/FacilityCard";
 const FacititySection = () => {
     const { data: facitityData } = useGetFacilityQuery(undefined);
     return (
-        <section className="grid grid-cols-3">
+        <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
             {facitityData?.data
                 .filter((facitity: IFacility) => facitity?.isDeleted === false)
                 .filter((facitity: IFacility) => facitity?.name.includes(""))
+                .slice(0, 6)
                 .map((facitity: IFacility) => (
                     <section>
                         <FacilityCard facitity={facitity} />
